b0cfbda182ff00300e493775db49faf3356895fa
Shubham Shah <shubhamshahrising@gmail.com>
2023-07-11 09:42:40 +0530

    fix: issue with search value

--- src/transformRequest.js
+++ src/transformRequest.js
@@ -1,9 +1,4 @@
-import {
-  ALL_LABEL,
-  RESULT_COMPONENT_ID,
-  SEARCH_COMPONENT_ID,
-  TABS_COMPONENT_ID,
-} from "./constants";
+import { ALL_LABEL, RESULT_COMPONENT_ID, TABS_COMPONENT_ID } from "./constants";
 
 export const transformRequest = (req) => {
   const body = JSON.parse(req.body);
@@ -15,16 +10,11 @@ export const transformRequest = (req) => {
         componentQuery.value[0] === ALL_LABEL
     )
   );
+
   body.query = body.query.map((componentQuery) => {
     // handle when no search value is there. The component is making a suggestion query
     if (componentQuery.id === RESULT_COMPONENT_ID) {
-      const searchQuery = body.query.find(
-        (cQ) => cQ.id === SEARCH_COMPONENT_ID
-      );
-      if (
-        (searchQuery && !searchQuery.value && showAllResults) ||
-        (!searchQuery && showAllResults)
-      ) {
+      if (showAllResults) {
         const from = componentQuery.from;
         delete componentQuery.from;
         return {
